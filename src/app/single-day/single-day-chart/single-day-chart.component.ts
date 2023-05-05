fa5f925e54809f849d349c661c55357b998ac1e7
Neil <neil.coder@gmail.com>
2023-05-05 09:19:49 +0200

    using dynamic width now

--- src/app/single-day/single-day-chart/single-day-chart.component.ts
+++ src/app/single-day/single-day-chart/single-day-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/core';
 import { Color, ScaleType } from '@swimlane/ngx-charts';
 
 
@@ -26,7 +26,7 @@ showYAxis = true;
 gradient = false;
 showLegend = false;
 showXAxisLabel = true;
-xAxisLabel = 'Country';
+xAxisLabel = 'Category';
 showYAxisLabel = true;
 yAxisLabel = 'Population';
 // colorScheme = {
@@ -51,21 +51,29 @@ colorScheme: Color = {
   }
   private _data = '';
 
-  constructor() {
+  @ViewChild('myElement')
+  myElement!: ElementRef;
 
-    // Assign the data to the data source for the table to render
-    // this.dataSource = new MatTableDataSource(this.items);
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+
+
+  ngAfterViewInit() {
+    const width = this.renderer.selectRootElement(this.myElement.nativeElement).offsetWidth;
+    console.log('Width:', width);
+    this.updateViewWidth(width);
   }
+  
 
   populateItems(inData: any) {
 
     this.items = [];
-    this.items.push(this.createItemFromSource(inData.data.cases.total, 'cases'));
+    // this.items.push(this.createItemFromSource(inData.data.cases.total, 'cases'));
     this.items.push(this.createItemFromSource(inData.data.outcomes.death.total, 'death'));
     this.items.push(this.createItemFromSource(inData.data.outcomes.hospitalized.currently, 'hospitalized'));
     this.items.push(this.createItemFromSource(inData.data.outcomes.hospitalized.in_icu.currently, 'in ICU'));
     this.items.push(this.createItemFromSource(inData.data.outcomes.hospitalized.on_ventilator.currently, 'on ventilator'));
-    this.items.push(this.createItemFromSource(inData.data.testing.total, 'testing'));
+    // this.items.push(this.createItemFromSource(inData.data.testing.total, 'testing'));
 
     // this.dataSource = new MatTableDataSource(this.items);
 
@@ -85,6 +93,10 @@ colorScheme: Color = {
 
   onSelect(event: any) {
     console.log(event);
+  }
+
+  updateViewWidth(newWidth: number) {
+    this.view.splice(0, 1, newWidth);
   }
 
 }
