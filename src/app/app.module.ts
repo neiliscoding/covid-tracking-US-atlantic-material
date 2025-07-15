84e6b9839d299e23e453324cb1dde2aa7d6e4217
neiliscoding <neil.coder@gmail.com>
2025-07-15 17:52:22 +0200

    update to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { SingleDayTableComponent } from './single-day/single-day-table/single-day-table.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
@@ -22,34 +22,27 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { SingleDayChartComponent } from './single-day/single-day-chart/single-day-chart.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    SingleDayComponent,
-    SingleDayDatepickerComponent,
-    SingleDayTableComponent,
-    SingleDayChartComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatNativeDateModule,
-    MatDatepickerModule,
-    MatFormFieldModule,
-    MatNativeDateModule,
-    MatInputModule,
-    MatTableModule,
-    MatTooltipModule,
-    MatPaginatorModule,
-    FormsModule,
-    NgxChartsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        SingleDayComponent,
+        SingleDayDatepickerComponent,
+        SingleDayTableComponent,
+        SingleDayChartComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatNativeDateModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatNativeDateModule,
+        MatInputModule,
+        MatTableModule,
+        MatTooltipModule,
+        MatPaginatorModule,
+        FormsModule,
+        NgxChartsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
